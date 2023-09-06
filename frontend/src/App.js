946f49d51a0f792cbe68aed37e7c508724650702
Yui Yamaguma <yui593ktoy@gmail.com>
2023-09-06 13:18:06 +0900

    登録画面追加

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
-import "./App.css";
 import { Route, Routes } from "react-router-dom";
+import "./App.css";
 
+import EatoutRegistration from "./pages/eatoutRegistration";
+import GradientRegistration from "./pages/gradientRegistration";
 import Home from "./pages/home";
 
 function App() {
@@ -11,6 +13,8 @@ function App() {
       <Route path="/home" element={<Home />} />
       <Route path="/compare" element={<Home />} />
       <Route path="/gradient" element={<Home />} />
+      <Route path="/registration/shopping" element={<GradientRegistration />} />
+      <Route path="/registration/eatout" element={<EatoutRegistration />} />
     </Routes>
   );
 }
