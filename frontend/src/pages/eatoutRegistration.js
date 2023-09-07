742c8c9e79557e9cfc251d7a3b3a56adb4d41cc2
Yui Yamaguma <yui593ktoy@gmail.com>
2023-09-07 09:57:54 +0900

    画面遷移追加

--- frontend/src/pages/eatoutRegistration.js
+++ frontend/src/pages/eatoutRegistration.js
@@ -10,16 +10,16 @@ function EatoutRegistration() {
       <div className="flex h-screen">
         <Sidebar />
         <div className="flex justify-center w-screen">
-          <form  className="flex flex-col justify-center w-1/3 gap-4 text-3xl leading-loose">
+          <form action="/" className="flex flex-col justify-center w-2/5 gap-4 text-3xl leading-loose">
             <p>日付　<input type="date" id="date" name="date" required /></p>
-            <p>金額　<input typte="number" step="1" id="price" name="price" required className="border-2" />円</p>
+            <p>金額　<input typte="number" step="1" id="price" name="price" required className="pr-5 pl-5 rounded-full border-2" />円</p>
             <div>
               用途　<select id="purpose" name="purpose" className="border-2">
                 <option>飲み会</option>
                 <option>その他</option>
               </select>
             </div>
-            <p className="flex justify-end"><input type="submit" value="登録" method="POST" className="border-2" /></p>
+            <p className="flex justify-end"><input type="submit" value="登録" method="POST" className="pr-5 pl-5 rounded-full border-2" /></p>
           </form>
         </div>
       </div>
--- frontend/src/pages/gradientRegistration.js
+++ frontend/src/pages/gradientRegistration.js
@@ -10,18 +10,18 @@ function GradientRegistration() {
       <div className="flex h-screen">
         <Sidebar />
         <div className="flex justify-center w-screen">
-          <form  className="flex flex-col justify-center gap-4 text-3xl leading-loose">
-            <p>名前　<input type="text" id="name" name="name" required className="border-2" /></p>
-            <p>カテゴリー　<input type="text" id="category" name="catgory" required className="border-2" /></p>
+          <form action="/" className="flex flex-col justify-center gap-4 text-3xl leading-loose">
+            <p>名前　<input type="text" id="name" name="name" required className="pr-5 pl-5 rounded-full border-2" /></p>
+            <p>カテゴリー　<input type="text" id="category" name="catgory" required className="pr-5 pl-5 rounded-full border-2" /></p>
             <div className="flex gap-10">
-              <p>購入日　<input type="date" id="date" name="date" required /></p>
-              <p>賞味期限／消費期限　<input type="date" id="limit" name="limit" required /></p>
+              <p>購入日　<input type="date" id="date" name="date" className="pr-5 pl-5 rounded-full" required /></p>
+              <p>賞味期限／消費期限　<input type="date" id="limit" name="limit" className="pr-5 pl-5 rounded-full" required /></p>
             </div>
             <div className="flex gap-10">
-              <p>金額　<input typte="number" id="price" name="price" required className="border-2" />円</p>
+              <p>金額　<input typte="number" id="price" name="price" required className="pr-5 pl-5 rounded-full border-2" />円</p>
               <p>
-                量　<input type="number" id="amount" name="price" required className="border-2" />
-                <select id="unit" name="unit" className="border-2">
+                量　<input type="number" id="amount" name="price" required className="pr-8 pl-8 rounded-full border-2" />
+                <select id="unit" name="unit" className="pr-5 pl-5 rounded-full border-2">
                   <option>個</option>
                   <option>g</option>
                   <option>ml</option>
@@ -29,9 +29,9 @@ function GradientRegistration() {
               </p>
             </div>
             <div className="w-full">
-              メモ　<textarea id="memo" name="memo" className="border-2 w-full" />
+              メモ　<textarea id="memo" name="memo" className="pr-5 pl-5 rounded-3xl border-2 w-full" />
             </div>
-            <p className="flex justify-end"><input type="submit" value="登録" method="POST" className="border-2" /></p>
+            <p className="flex justify-end"><input type="submit" value="登録" method="POST" className="pr-5 pl-5 rounded-full border-2" /></p>
           </form>
         </div>
       </div>
