de26b975529625ed12e74879407ef9ccccebfaa7
Keisuke05410 <keisuke3469@gmail.com>
2023-09-07 23:14:21 +0900

    食材一覧データ取得

--- frontend/src/pages/foodList.jsx
+++ frontend/src/pages/foodList.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom"; // useNavigateをインポート
 import Header from "../components/header";
 import Sidebar from "../components/sidebar";
 import { InformationCircleIcon } from "@heroicons/react/24/outline";
@@ -15,41 +16,17 @@ import {
   Tooltip,
   Typography,
 } from "@material-tailwind/react";
+import axios from "axios";
 
-const foodListData = [
-  {
-    name: "トマト",
-    category: "野菜",
-    purchaseDate: "2023/01/01",
-    expiredDate: "2023/01/01",
-    price: 100,
-    quantity: 1,
-    unit: "個",
-    memo: "おいしいらしい",
-    stock: 80,
-  },
-  {
-    name: "キャベツ",
-    category: "野菜",
-    purchaseDate: "2023/01/02",
-    expiredDate: "2023/01/02",
-    price: 200,
-    quantity: 2,
-    unit: "玉",
-    memo: "甘くておいしい",
-    stock: 30,
-  },
-  {
-    name: "レタス",
-    category: "野菜",
-    purchaseDate: "2023/01/03",
-    expiredDate: "2023/01/03",
-    price: 150,
-    quantity: 1,
-    unit: "個",
-    memo: "シャキシャキしている",
-    stock: 40,
-  },
+const convertCategory = [
+  "その他",
+  "野菜",
+  "肉",
+  "魚",
+  "穀物",
+  "調味料",
+  "加工食品",
+  "飲料水",
 ];
 
 const FoodListHeader = () => {
@@ -69,86 +46,109 @@ const FoodListHeader = () => {
   );
 };
 
-const FoodListItem = ({ food, handleRegisterPopoverInput }) => {
+const FoodListItem = ({ food }) => {
+  const navigate = useNavigate(); // useNavigateを使用して、navigate関数を取得
+
+  const handleItemClick = () => {
+    navigate("/gradientChange", { state: { food } }); // navigate関数を使用して、ページ遷移を行う
+  };
+
   return (
-    <a href="#" className="text-initial">
-      <ListItem>
-        <div className="flex items-center w-full">
-          <div className="w-1/6 text-center">{food.name}</div>
-          <div className="w-1/6 text-center">{food.category}</div>
-          <div className="w-1/6 text-center">{food.purchaseDate}</div>
-          <div className="w-1/6 text-center">{food.expiredDate}</div>
-          <div className="w-1/6 text-center">￥{food.price}</div>
-          <div className="w-1/6 text-center">
-            {food.quantity}
-            {food.unit}
-          </div>
-          <div className="w-1/12 mr-5 flex justify-center items-center">
-            <Tooltip
-              placement="bottom"
-              className="border border-blue-gray-50 bg-white px-4 py-3 shadow-xl shadow-black/10 "
-              content={
-                <div className="w-80">
-                  <Typography
-                    variant="small"
-                    color="blue-gray"
-                    className="font-normal opacity-80"
-                  >
-                    {food.memo}
-                  </Typography>
-                </div>
-              }
-            >
-              <InformationCircleIcon className="h-4 w-4 text-center" />
-            </Tooltip>
-          </div>
-          <div className="w-2/5 flex justify-between items-center">
-            <div className="w-full">
-              <div className="mb-2 flex items-center justify-between gap-4">
-                {console.log("rendering")}
-                <Typography color="blue-gray" variant="h6">
-                  残量
-                </Typography>
-                <Typography color="blue-gray" variant="h6">
-                  {food.stock}%
+    <ListItem onClick={handleItemClick} className="cursor-pointer">
+      <div className="flex items-center w-full">
+        <div className="w-1/6 text-center">{food.name}</div>
+        <div className="w-1/6 text-center">
+          {convertCategory[food.category]}
+        </div>
+        <div className="w-1/6 text-center">{food.Date}</div>
+        <div className="w-1/6 text-center">{food.expiry_date}</div>
+        <div className="w-1/6 text-center">￥{food.price}</div>
+        <div className="w-1/6 text-center">
+          {food.amount}
+          {food.unit}
+        </div>
+        <div className="w-1/12 mr-5 flex justify-center items-center">
+          <Tooltip
+            placement="bottom"
+            className="border border-blue-gray-50 bg-white px-4 py-3 shadow-xl shadow-black/10 "
+            content={
+              <div className="w-80">
+                <Typography
+                  variant="small"
+                  color="blue-gray"
+                  className="font-normal opacity-80"
+                >
+                  {food.memo}
                 </Typography>
               </div>
-              <Progress value={food.stock} max={100} />
-            </div>
-            <Popover placement="bottom">
-              <PopoverHandler>
-                <Button className="w-40 ml-10">残量設定</Button>
-              </PopoverHandler>
-              {/* 残量の変更をする部分 */}
-              <PopoverContent className="w-96">
-                <Typography variant="h6" color="blue-gray" className="mb-6">
-                  残量を設定してください
-                </Typography>
-                <div className="flex gap-2">
-                  <Input label="残量(%)" type="number" />
-                  <Button
-                    variant="gradient"
-                    onClick={() => handleRegisterPopoverInput()}
-                  >
-                    OK
-                  </Button>
-                </div>
-              </PopoverContent>
-            </Popover>
-          </div>
+            }
+          >
+            <InformationCircleIcon className="h-4 w-4 text-center" />
+          </Tooltip>
         </div>
-      </ListItem>
-    </a>
+        <div className="w-2/5 flex justify-between items-center">
+          <div className="w-full">
+            <div className="mb-2 flex items-center justify-between gap-4">
+              <Typography color="blue-gray" variant="h6">
+                残量
+              </Typography>
+              <Typography color="blue-gray" variant="h6">
+                {food.Remaining}%
+              </Typography>
+            </div>
+            <Progress value={food.Remaining} max={100} />
+          </div>
+          <Popover placement="bottom">
+            <PopoverHandler>
+              <Button className="w-40 ml-10">残量設定</Button>
+            </PopoverHandler>
+            {/* 残量の変更をする部分 */}
+            <PopoverContent className="w-96">
+              <Typography variant="h6" color="blue-gray" className="mb-6">
+                残量を設定してください
+              </Typography>
+              <div className="flex gap-2">
+                <Input label="残量(%)" type="number" />
+                <Button variant="gradient">OK</Button>
+              </div>
+            </PopoverContent>
+          </Popover>
+        </div>
+      </div>
+    </ListItem>
   );
 };
 
 const FoodList = () => {
-  const [registerPopoverInput, setRegisterPopoverInput] = useState(true);
+  const [foodListData, setFoodListData] = useState(null);
 
-  //   ここで残量が変更された時の処理(未実装)
-  const handleRegisterPopoverInput = () => {
-    setRegisterPopoverInput(!registerPopoverInput);
-  };
+  const baseUrl = "http://127.0.0.1:8000";
+  useEffect(() => {
+    const fetchFoodList = async () => {
+      try {
+        const response = await axios.get(baseUrl + "/food_db", {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        });
+        console.log(response);
+        if (response.status === 200) {
+          console.log(response.data);
+          setFoodListData(response.data);
+        } else {
+          console.log("バックエンドからのエラー");
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchFoodList();
+  }, []);
+
+  useEffect(() => {
+    console.log(foodListData);
+  }, [foodListData]);
 
   return (
     <>
@@ -159,13 +159,10 @@ const FoodList = () => {
           <Card>
             <List>
               <FoodListHeader />
-              {foodListData.map((food, index) => (
-                <FoodListItem
-                  key={index}
-                  food={food}
-                  handleRegisterPopoverInput={handleRegisterPopoverInput}
-                />
-              ))}
+              {foodListData &&
+                Object.values(foodListData).map((food, index) => (
+                  <FoodListItem key={index} food={food} />
+                ))}
             </List>
           </Card>
         </div>
