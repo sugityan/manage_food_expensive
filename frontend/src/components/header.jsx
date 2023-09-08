f09e9f4418f6313cb7c2e1de02e97adb372cefcd
sugityan <baketu427@gmail.com>
2023-09-08 11:46:47 +0900

    login page nav

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -4,6 +4,16 @@ import { Navbar, Typography } from "@material-tailwind/react";
 const Header = () => {
   const navList = (
     <ul className="mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+        <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >
+        <a href="login" className="flex items-center">
+          loginページ
+        </a>
+      </Typography>
       <Typography
         as="li"
         variant="small"
@@ -45,7 +55,7 @@ const Header = () => {
           href="home"
           className="mr-4 cursor-pointer py-1.5 text-2xl font-bold"
         >
-          Food Server(仮)
+          Food Server
         </Typography>
         <div className="flex items-center gap-4">
           <div className="mr-4 hidden lg:block">{navList}</div>
